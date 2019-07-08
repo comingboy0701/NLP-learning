91c9e62509860dd71f246ab5702a0f14ea3b258e
comingboy <760855003@qq.com>
2019-07-08 22:24:41 +0800

    v201907082224-coming

--- assignments_02/scrapy_beijing_sub.py
+++ assignments_02/scrapy_beijing_sub.py
@@ -7,14 +7,17 @@ import time
 from fake_useragent import UserAgent
 import random
 import re
+from urllib import parse
+from urllib.request import quote
 
-url ='https://baike.baidu.com/item/%E5%8C%97%E4%BA%AC%E5%9C%B0%E9%93%81/408485'
 
-def get_one_url(url):
+def get_one_page(url):
    try:
        ua = UserAgent()
        headers = {'User-Agent': ua.random}
-       reponse = requests.get(url, headers=headers, timeout=2)
+       IP = {'http':random.choice(ips)} #指定对应的 IP 进行访问网址
+       reponse = requests.get(url, headers=headers, proxies=IP,timeout=2 )
+       print(reponse)
        if reponse.status_code == 200:
            return reponse.content
        return None
@@ -22,26 +25,33 @@ def get_one_url(url):
        return None
 
 
-
 def get_one_parse(url):
-    html = str(get_one_url(url).decode("utf-8"))
-    print(html)
-    pattern_url = re.compile(r'target="_blank" href="(.*?)">北京地铁', re.S)
-
+    
+    print(get_one_page(url))
+    html = str(get_one_page(url).decode("utf-8"))
+    pattern_url = re.compile(r'target=_blank href="(.*?)"')
     url_sub = re.findall(pattern_url, html)
-
-    sub =
-    stations =
-
-    print(html)
-
-
+    url_cn = [parse.unquote(url) for url in url_sub ]
+    url_cn_rm = list(set([url for url in url_cn if '线' in url]))
+    return url_cn_rm
 
 def write(sub):
 
+def read_ip():
+    with open(r'.\ip.txt','r') as f:
+       lines = f.readlines()
+       # 我们去掉lines每一项后面的\n\r之类的空格
+       # 生成一个新的列表！
+       ips = list(map(lambda x:x.strip(),[line for line in lines]))
+       
 
-
-def main()
+def main():
+    
+    read_ip()
+    
+    url ='https://baike.baidu.com/item/%E5%8C%97%E4%BA%AC%E5%9C%B0%E9%93%81/408485'
+    
+    get_url = get_one_parse(url)
 
     get_url()
 
